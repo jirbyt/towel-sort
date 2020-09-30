ccbbcf4840be52fd0bb4e3399af5d7567d2a7e41
jirbyt <jirbyt@yandex.ru>
2020-09-30 21:39:49 +0300

    init: start towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+if (Array.isArray(matrix) && matrix.length != 0){
+let arrNew =[]; 
+let arrNewer =[]; 
+let strNew='';
+for(let i=0; i<matrix.length; i++){
+i % 2 == 0 ?  strNew+= matrix[i].join(',') +',' : strNew+= matrix[i].reverse().join(',') +',';
+}
+arrNew = strNew.split(',');
+for(let i = 0; i<arrNew.length-1; i++){
+arrNewer[i]=+arrNew[i];
+}
+return arrNewer;
+}
+else return [] 
 }
